ed8e4741bbdb5dcf9fe6a6507f1a1d648a9566de
Jefferson Gomes <jeffsilvaygo@gmail.com>
2025-12-28 17:01:59 -0300

    feat(data): add enums for datasus api codes

---
+++ data/enums.py
@@ -0,0 +1,31 @@
+SEXO_MAP = {
+    "M": "Masculino",
+    "F": "Feminino",
+    "I": "Ignorado"
+}
+
+RACA_MAP = {
+    "1": "Branca",
+    "2": "Preta",
+    "3": "Amarela",
+    "4": "Parda",
+    "5": "Indígena",
+    "9": "Ignorado"
+}
+
+GESTANTE_MAP = {
+    "1": "1º Trimestre",
+    "2": "2º Trimestre",
+    "3": "3º Trimestre",
+    "4": "Idade gestacional ignorada",
+    "5": "Não",
+    "6": "Não se aplica",
+    "9": "Ignorado"
+}
+
+TIPO_NOT_MAP = {
+    "1": "Negativa",
+    "2": "Individual",
+    "3": "Surto",
+    "4": "Agregado"
+}
