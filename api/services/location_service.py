6c848cbd33791786a5260c8d6625c2be1f5286fb
Jefferson Gomes <jeffsilvaygo@gmail.com>
2026-01-02 22:31:31 -0300

    feat(location): add uf and municipio translation service

---
+++ api/services/location_service.py
@@ -0,0 +1,26 @@
+from data.lookups.loader import load_ufs, load_municipios
+
+ufs_by_sigla, ufs_by_id = load_ufs()
+municipios = load_municipios()
+
+
+def translate_uf(sigla: str) -> dict | None:
+    """
+    Retorna dados da UF a partir da sigla
+    """
+    uf = ufs_by_sigla.get(sigla.upper())
+    return str(uf["id"]) if uf else None
+
+
+def translate_uf_by_code(codigo: str) -> dict | None:
+    """
+    Recebe código IBGE da UF e retorna o objeto completo
+    """
+    return ufs_by_id.get(str(codigo))
+
+
+def translate_municipio(codigo: str) -> dict | None:
+    """
+    Recebe código IBGE do município
+    """
+    return municipios.get(str(codigo))
