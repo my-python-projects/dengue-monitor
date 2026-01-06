0c6700e8e144c3191056e4795fba05d2b6de5e12
Jefferson Gomes <jeffsilvaygo@gmail.com>
2026-01-05 22:17:19 -0300

    refactor(lookup): adjust municipios.json loading

--- data/lookups/loader.py
+++ data/lookups/loader.py
@@ -8,18 +8,31 @@ def load_ufs():
     with open(BASE_PATH / "ufs.json", encoding="utf-8") as f:
         data = json.load(f)
 
-    by_sigla = {}
     by_id = {}
+    by_sigla = {}
+    # by_name = {}
 
     for uf in data:
-        by_sigla[uf["sigla"]] = uf
         by_id[str(uf["id"])] = uf
+        by_sigla[uf["sigla"]] = uf
+        # by_name[uf["nome"]] = uf
+        
 
-    return by_sigla, by_id
+    return by_id , by_sigla 
 
 
 def load_municipios():
     with open(BASE_PATH / "municipios.json", encoding="utf-8") as f:
         data = json.load(f)
 
-    return {str(m["id"]): m for m in data}
+    municipios_by_6digits = {}
+    for m in data:
+        codigo_7 = str(m["id"])          # ex: "3106207"
+        if len(codigo_7) == 7:
+            codigo_6 = codigo_7[:6]      # ex: "310620"
+            municipios_by_6digits[codigo_6] = m
+        else:
+            # Caso raro: código não tem 7 dígitos → use como está
+            municipios_by_6digits[codigo_7] = m
+
+    return municipios_by_6digits
